e86e45843fc974f82f0b1ea697d4e31ba559c44d
Piotr Dyraga <piotr.dyraga@keep.network>
2021-07-21 15:28:10 +0200

    Added Solidity docs to VendingMachine contract

--- contracts/vending/VendingMachine.sol
+++ contracts/vending/VendingMachine.sol
@@ -9,6 +9,17 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@thesis/solidity-contracts/contracts/token/IReceiveApproval.sol";
 import "../token/T.sol";
 
+/// @title T token vending machine
+/// @notice Contract implements a special lending protocol to enable KEEP/NU
+///         token holders to wrap their tokens as collateral and borrow T tokens
+///         against that collateral based on the wrap ratio. This will go on
+///         indefinitely and enable NU and KEEP token holders to join T network
+///         without needing to buy or sell any assets. Logistically, anyone
+///         holding NU or KEEP can wrap those assets in order to receive T.
+///         They can also unwrap T in order to go back to the underlying asset.
+///         There is a separate instance of this contract deployed for KEEP
+///         holders and a separate instance of this contract deployed for NU
+///         holders.
 contract VendingMachine is Ownable, IReceiveApproval {
     using SafeERC20 for IERC20;
     using SafeERC20 for T;
@@ -16,16 +27,25 @@ contract VendingMachine is Ownable, IReceiveApproval {
     /// @notice Divisor for precision purposes, used to represent fractions.
     uint256 public constant FLOATING_POINT_DIVISOR = 1e18;
 
+    /// @notice The token being wrapped to T (KEEP/NU).
     IERC20 public immutable wrappedToken;
+    
+    /// @notice T token contract.
     T public immutable tToken;
 
-    // when wrapping:
-    // x [T] = * amount [source token] * ratio / FLOATING_POINT_DIVISOR
-    //
-    // when unwrapping:
-    // x [source token] = amount [T] * FLOATING_POINT_DIVISOR / ratio
+    /// @notice The ratio with which T token is borrowed based on the provided
+    ///         token being wrapped (KEEP/NU).
+    ///
+    ///         When wrapping:
+    ///           x [T] = amount [KEEP/NU] * ratio / FLOATING_POINT_DIVISOR
+    ///  
+    ///         When unwrapping:
+    ///           x [KEEP/NU] = amount [T] * FLOATING_POINT_DIVISOR / ratio
     uint256 public ratio;
 
+    /// @notice The total balance of wrapped tokens for the given holder
+    ///         account. Only holders that have previously wrapped KEEP/NU to T
+    ///         can unwrap, up to the amount previously wrapped.
     mapping(address => uint256) public wrappedBalance;
 
     event Wrapped(
@@ -49,10 +69,25 @@ contract VendingMachine is Ownable, IReceiveApproval {
         ratio = _ratio;
     }
 
+    /// @notice Wraps the given amount of the token (KEEP/NU) as collateral and
+    ///         borrows T token proportionally to the amount being wrapped and
+    ///         the wrap ratio. The token holder needs to have at least the
+    ///         given amount of the wrapped token (KEEP/NU) approved to transfer
+    ///         to the Vending Machine before calling this function.
+    /// @param amount The amount of KEEP/NU to be wrapped
     function wrap(uint256 amount) external {
         _wrap(msg.sender, amount);
     }
 
+    /// @notice Wraps the given amount of the token (KEEP/NU) as collateral and
+    ///         borrows T token proportionally to the amount being wrapped and
+    ///         the wrap ratio. This is a shortcut to `wrap` function allowing
+    ///         to avoid a separate approval transaction. Only KEEP/NU token
+    ///         is allowed as a caller, so please call this function via
+    ///         token's `approveAndCall`.
+    /// @param from Caller's address, must be the same as `wrappedToken` field
+    /// @param amount The amount of KEEP/NU to be wrapped
+    /// @param token Token's address, must be the same as `wrappedToken` field
     function receiveApproval(
         address from,
         uint256 amount,
@@ -70,6 +105,14 @@ contract VendingMachine is Ownable, IReceiveApproval {
         _wrap(from, amount);
     }
 
+    /// @notice Unwraps the given amount of T back to the collateral (KEEP/NU)
+    ///         based on the wrap ratio. Can only be called by a token holder
+    ///         who previously wrapped their tokens. The token holder can not
+    ///         unwrap more tokens than they have originally wrapped. The token
+    ///         holder needs to have at least the given amount of T token
+    ///         approved to transfer to the Vending Machine before calling this
+    ///         function.
+    /// @param amount The amount of T to unwrap back to the collateral (KEEP/NU)
     function unwrap(uint256 amount) external {
         _unwrap(msg.sender, amount);
     }
