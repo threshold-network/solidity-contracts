ec8599623fbb3e2ffc6c77f79e7e048b9e4f0470
Piotr Dyraga <piotr.dyraga@keep.network>
2021-08-06 11:23:11 +0200

    Implemented unit tests for delegateBySig function

--- test/helpers/contract-test-helpers.js
+++ test/helpers/contract-test-helpers.js
@@ -7,5 +7,11 @@ async function lastBlockNumber() {
   return (await ethers.provider.getBlock("latest")).number
 }
 
+async function lastBlockTime() {
+  return (await ethers.provider.getBlock("latest")).timestamp
+}
+
 module.exports.to1e18 = to1e18
 module.exports.lastBlockNumber = lastBlockNumber
+module.exports.lastBlockTime = lastBlockTime
+module.exports.ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
--- test/token/T.test.js
+++ test/token/T.test.js
@@ -3,11 +3,18 @@ const { expect } = require("chai")
 const {
   to1e18,
   lastBlockNumber,
+  lastBlockTime,
   ZERO_ADDRESS,
 } = require("../helpers/contract-test-helpers")
 
 describe("T token", () => {
   const initialBalance = to1e18(1000000)
+  let deployer
+  let tokenHolder
+  let tokenRecipient
+  let delegatee
+  let delegatee2
+  let thirdParty
 
   beforeEach(async () => {
     ;[
@@ -48,31 +55,31 @@ describe("T token", () => {
     })
   })
 
-  describe("delegate", () => {
+  const describeDelegate = (getDelegator, doDelegate) => {
     context("when delegated to someone else", () => {
+      let delegator
       let tx
 
       beforeEach(async () => {
-        tx = await t.connect(tokenHolder).delegate(delegatee.address)
+        delegator = getDelegator()
+        tx = await doDelegate(delegatee.address)
       })
 
       it("should update current votes", async () => {
-        expect(await t.getCurrentVotes(tokenHolder.address)).to.equal(0)
+        expect(await t.getCurrentVotes(delegator.address)).to.equal(0)
         expect(await t.getCurrentVotes(delegatee.address)).to.equal(
           initialBalance
         )
       })
 
       it("should update delegatee address", async () => {
-        expect(await t.delegates(tokenHolder.address)).to.equal(
-          delegatee.address
-        )
+        expect(await t.delegates(delegator.address)).to.equal(delegatee.address)
       })
 
       it("should emit DelegateChanged event", async () => {
         await expect(tx)
           .to.emit(t, "DelegateChanged")
-          .withArgs(tokenHolder.address, ZERO_ADDRESS, delegatee.address)
+          .withArgs(delegator.address, ZERO_ADDRESS, delegatee.address)
       })
 
       it("should emit DelegateVotesChanged", async () => {
@@ -83,57 +90,59 @@ describe("T token", () => {
     })
 
     context("when self-delegated", () => {
+      let delegator
       let tx
 
       beforeEach(async () => {
-        tx = await t.connect(tokenHolder).delegate(tokenHolder.address)
+        delegator = getDelegator()
+        tx = await doDelegate(delegator.address)
       })
 
       it("should update current votes", async () => {
-        expect(await t.getCurrentVotes(tokenHolder.address)).to.equal(
+        expect(await t.getCurrentVotes(delegator.address)).to.equal(
           initialBalance
         )
       })
 
       it("should update delegatee address", async () => {
-        expect(await t.delegates(tokenHolder.address)).to.equal(
-          tokenHolder.address
-        )
+        expect(await t.delegates(delegator.address)).to.equal(delegator.address)
       })
 
       it("should emit DelegateChanged event", async () => {
         await expect(tx)
           .to.emit(t, "DelegateChanged")
-          .withArgs(tokenHolder.address, ZERO_ADDRESS, tokenHolder.address)
+          .withArgs(delegator.address, ZERO_ADDRESS, delegator.address)
       })
 
       it("should emit DelegateVotesChanged", async () => {
         await expect(tx)
           .to.emit(t, "DelegateVotesChanged")
-          .withArgs(tokenHolder.address, 0, initialBalance)
+          .withArgs(delegator.address, 0, initialBalance)
       })
     })
 
     context("when delegated multiple times", () => {
+      let delegator
       let block1
       let block2
       let block3
       let block4
 
       beforeEach(async () => {
-        await t.connect(tokenHolder).delegate(delegatee.address)
+        delegator = getDelegator()
+        await doDelegate(delegatee.address)
         block1 = await lastBlockNumber()
-        await t.connect(tokenHolder).delegate(delegatee2.address)
+        await doDelegate(delegatee2.address)
         block2 = await lastBlockNumber()
-        await t.connect(tokenHolder).delegate(delegatee.address)
+        await doDelegate(delegatee.address)
         block3 = await lastBlockNumber()
-        await t.connect(tokenHolder).delegate(tokenHolder.address)
+        await doDelegate(delegator.address)
         block4 = await lastBlockNumber()
-        await t.connect(tokenHolder).delegate(delegatee2.address)
+        await doDelegate(delegatee2.address)
       })
 
       it("should update current votes", async () => {
-        expect(await t.getCurrentVotes(tokenHolder.address)).to.equal(0)
+        expect(await t.getCurrentVotes(delegator.address)).to.equal(0)
         expect(await t.getCurrentVotes(delegatee.address)).to.equal(0)
         expect(await t.getCurrentVotes(delegatee2.address)).to.equal(
           initialBalance
@@ -141,31 +150,147 @@ describe("T token", () => {
       })
 
       it("should keep track of prior votes", async () => {
-        expect(await t.getPriorVotes(tokenHolder.address, block1)).to.equal(0)
+        expect(await t.getPriorVotes(delegator.address, block1)).to.equal(0)
         expect(await t.getPriorVotes(delegatee.address, block1)).to.equal(
           initialBalance
         )
         expect(await t.getPriorVotes(delegatee2.address, block1)).to.equal(0)
 
-        expect(await t.getPriorVotes(tokenHolder.address, block2)).to.equal(0)
+        expect(await t.getPriorVotes(delegator.address, block2)).to.equal(0)
         expect(await t.getPriorVotes(delegatee.address, block2)).to.equal(0)
         expect(await t.getPriorVotes(delegatee2.address, block2)).to.equal(
           initialBalance
         )
 
-        expect(await t.getPriorVotes(tokenHolder.address, block3)).to.equal(0)
+        expect(await t.getPriorVotes(delegator.address, block3)).to.equal(0)
         expect(await t.getPriorVotes(delegatee.address, block3)).to.equal(
           initialBalance
         )
         expect(await t.getPriorVotes(delegatee2.address, block3)).to.equal(0)
 
-        expect(await t.getPriorVotes(tokenHolder.address, block4)).to.equal(
+        expect(await t.getPriorVotes(delegator.address, block4)).to.equal(
           initialBalance
         )
         expect(await t.getPriorVotes(delegatee.address, block4)).to.equal(0)
         expect(await t.getPriorVotes(delegatee2.address, block4)).to.equal(0)
       })
     })
+  }
+
+  describe("delegate", () => {
+    describeDelegate(
+      () => {
+        return tokenHolder
+      },
+      async (delegatee) => {
+        return await t.connect(tokenHolder).delegate(delegatee)
+      }
+    )
+  })
+
+  describe("delegateBySig", async () => {
+    let yesterday
+    let tomorrow
+
+    let delegator
+
+    beforeEach(async () => {
+      const lastBlockTimestamp = await lastBlockTime()
+      yesterday = lastBlockTimestamp - 86400 // -1 day
+      tomorrow = lastBlockTimestamp + 86400 // +1 day
+
+      // Hardhat creates SignerWithAddress instance that does not give access
+      // to private key. We need an access to private key so that we can construct
+      // ethers.utils.SigningKey as explained later.
+      delegator = await ethers.Wallet.createRandom()
+      await t.connect(deployer).mint(delegator.address, initialBalance)
+    })
+
+    describeDelegate(
+      () => {
+        return delegator
+      },
+      async (delegatee) => {
+        const signature = await getDelegation(delegatee, tomorrow)
+        return await t.delegateBySig(
+          delegator.address,
+          delegatee,
+          tomorrow,
+          signature.v,
+          signature.r,
+          signature.s
+        )
+      }
+    )
+
+    context("when delegation order expired", () => {
+      it("should revert", async () => {
+        const signature = await getDelegation(delegatee.address, yesterday)
+
+        await expect(
+          t.delegateBySig(
+            delegator.address,
+            delegatee.address,
+            yesterday,
+            signature.v,
+            signature.r,
+            signature.s
+          )
+        ).to.be.revertedWith("Delegation expired")
+      })
+    })
+
+    context("when delegation order has an invalid signature", () => {
+      it("should revert", async () => {
+        const signature = await getDelegation(delegatee.address, tomorrow)
+
+        await expect(
+          t.delegateBySig(
+            delegator.address,
+            delegatee.address,
+            tomorrow,
+            signature.v,
+            signature.s, // not r but s
+            signature.s
+          )
+        ).to.be.revertedWith("Invalid signature")
+      })
+    })
+
+    const getDelegation = async (delegatee, deadline) => {
+      // We use ethers.utils.SigningKey for a Wallet instead of
+      // Signer.signMessage to do not add '\x19Ethereum Signed Message:\n'
+      // prefix to the signed message. The '\x19` protection (see EIP191 for
+      // more details on '\x19' rationale and format) is already included in
+      // Delegation signed message and '\x19Ethereum Signed Message:\n'
+      // should not be used there.
+      const signingKey = new ethers.utils.SigningKey(delegator.privateKey)
+
+      const domainSeparator = await t.DOMAIN_SEPARATOR()
+      const delegationTypehash = await t.DELEGATION_TYPEHASH()
+      const nonce = await t.nonces(delegator.address)
+
+      const delegationDigest = ethers.utils.keccak256(
+        ethers.utils.solidityPack(
+          ["bytes1", "bytes1", "bytes32", "bytes32"],
+          [
+            "0x19",
+            "0x01",
+            domainSeparator,
+            ethers.utils.keccak256(
+              ethers.utils.defaultAbiCoder.encode(
+                ["bytes32", "address", "uint256", "uint256"],
+                [delegationTypehash, delegatee, nonce, deadline]
+              )
+            ),
+          ]
+        )
+      )
+
+      return ethers.utils.splitSignature(
+        await signingKey.signDigest(delegationDigest)
+      )
+    }
   })
 
   const describeTransfer = (doTransfer) => {
