9f3fd4cfc48ea9a7261dad07967ecb070bf7f6a1
David Núñez <david@nucypher.com>
2021-10-21 12:36:48 +0200

    Tokenholder voting power only needs to use token and staking contracts

--- contracts/governance/TokenholderGovernorVotes.sol
+++ contracts/governance/TokenholderGovernorVotes.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.0;
 import "./IVotingHistory.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
-/**
- * @dev Extension of {GovernorVotesQuorumFraction} for voting weight extraction from both liquid and staked T token positions.
- *      See OpenZeppelin's GovernorVotes and GovernorVotesQuorumFraction for reference.
- */
+/// @title TokenholderGovernorVotes
+/// @notice Tokenholder DAO voting power extraction
+/// @dev Extension of {GovernorVotesQuorumFraction} for voting power extraction
+///      from both liquid and staked T token positions. See OpenZeppelin's
+///      GovernorVotes and GovernorVotesQuorumFraction for reference.
 abstract contract TokenholderGovernorVotes is GovernorVotesQuorumFraction {
     IVotesHistory public immutable staking;
 
@@ -16,9 +17,18 @@ abstract contract TokenholderGovernorVotes is GovernorVotesQuorumFraction {
         staking = tStakingAddress;
     }
 
-    /**
-     * Read the voting weight from the token's built in snapshot mechanism (see {IGovernor-getVotes}).
-     */
+    /// @notice Read the voting weight from the snapshot mechanism in the token
+    ///         and staking contracts. For Tokenholder DAO, there are currently
+    ///         two voting power sources:
+    ///          - Liquid T, tracked by the T token contract
+    ///          - Stakes in the T network, tracked  by the T staking contract.
+    ///            Note that this also tracks legacy stakes (NU/KEEP); legacy
+    ///            stakes count for tokenholders' voting power, but not for the
+    ///            total voting power of the Tokenholder DAO
+    ///            (see {_getPastTotalSupply}).
+    /// @param account Tokenholder account in the T network
+    /// @param blockNumber The block number to get the vote balance at
+    /// @dev See {IGovernor-getVotes}
     function getVotes(address account, uint256 blockNumber)
         public
         view
@@ -29,18 +39,22 @@ abstract contract TokenholderGovernorVotes is GovernorVotesQuorumFraction {
         uint256 liquidVotes = token.getPastVotes(account, blockNumber);
         uint256 stakedVotes = staking.getPastVotes(account, blockNumber);
         return liquidVotes + stakedVotes;
-        // FIXME: Account for vending machines' balance of T
     }
 
+    /// @notice Compute the total voting power for Tokenholder DAO. Note how it
+    ///         only uses the token total supply as source, as native T tokens
+    ///         that are staked continue existing, but as deposits in the
+    ///         staking contract. However, legacy stakes can't contribute to the
+    ///         total voting power as they're already implicitly counted as part
+    ///         of Vending Machines' liquid balance; hence, we only need to read
+    ///         total voting power from the token.
+    /// @param blockNumber The block number to get the vote power at
     function _getPastTotalSupply(uint256 blockNumber)
         internal
         view
         returns (uint256)
     {
-        return
-            token.getPastTotalSupply(blockNumber) +
-            staking.getPastTotalSupply(blockNumber);
-        // FIXME: Account for vending machines' balance of T
+        return token.getPastTotalSupply(blockNumber);
     }
 
     /// @notice Compute the required amount of voting power to reach quorum
@@ -61,7 +75,13 @@ abstract contract TokenholderGovernorVotes is GovernorVotesQuorumFraction {
         return 10000;
     }
 
-    function quorumDenominator() public view virtual override returns (uint256) {
+    function quorumDenominator()
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
         return fractionDenominator();
     }
 }
