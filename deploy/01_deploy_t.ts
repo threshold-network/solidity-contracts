385e647864d2dfafa55ceff8beec17132f897fea
Michalina Cienciała <michalina.cienciala@keep.network>
2021-11-05 15:26:35 +0100

    Move minting of T to a separate script
    There is a chance that on mainnet we'd like to hold on with minting and
    delegate this responsibility to DAO.

--- deploy/01_deploy_t.ts
+++ deploy/01_deploy_t.ts
@@ -1,31 +1,14 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-import { BigNumber } from "ethers"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments } = hre
   const { deployer } = await getNamedAccounts()
-  const { execute, read, log } = deployments
 
   const T = await deployments.deploy("T", {
     from: deployer,
     log: true,
   })
-
-  // We're minting 10B T, which is the value of the T supply on the production
-  // environment.
-
-  await execute(
-    "T",
-    { from: deployer },
-    "mint",
-    deployer,
-    BigNumber.from(10).pow(28)
-  )
-
-  const tTotalSupply = await read("T", "totalSupply")
-
-  log("minted", tTotalSupply.toString(), "T")
 
   if (hre.network.tags.tenderly) {
     await hre.tenderly.verify({
---
+++ deploy/02_mint_t.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { BigNumber } from "ethers"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { deployer } = await getNamedAccounts()
+  const { execute, read, log } = deployments
+
+  const T = await deployments.get("T")
+
+  // We're minting 10B T, which is the value of the T supply on the production
+  // environment.
+
+  await execute(
+    "T",
+    { from: deployer },
+    "mint",
+    deployer,
+    BigNumber.from(10).pow(28)
+  )
+
+  const tTotalSupply = await read("T", "totalSupply")
+
+  log("minted", tTotalSupply.toString(), "T")
+
+  if (hre.network.tags.tenderly) {
+    await hre.tenderly.verify({
+      name: "T",
+      address: T.address,
+    })
+  }
+}
+
+export default func
+
+func.tags = ["MintT"]
