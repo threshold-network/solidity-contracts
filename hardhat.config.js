38f5aeba53365d10fdf0f2faea3d0665c7053108
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-11-15 15:11:24 +0100

    Configure Hardhat forking feature

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
+require("@keep-network/hardhat-helpers")
 
 module.exports = {
   solidity: {
@@ -9,6 +10,22 @@ module.exports = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      forking: {
+        // forking is enabled only if FORKING_URL env is provided
+        enabled: !!process.env.FORKING_URL,
+        // URL should point to a node with archival data (Alchemy recommended)
+        url: process.env.FORKING_URL || "",
+        // latest block is taken if FORKING_BLOCK env is not provided
+        blockNumber: process.env.FORKING_BLOCK
+          ? parseInt(process.env.FORKING_BLOCK)
+          : undefined,
+      },
+      tags: ["local"],
     },
   },
 }
