180ca7dc95c55be7dd8044809b91624a240c8e3d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-11-30 15:16:30 +0100

    Update script that deploys the VendingMachine for NU
    For NuCypher's VendingMachine deployment, we can't use the token
    `totalSupply()`, as it will report an incorrect figure for vending
    machine purposes. The correct number is the `currentPeriodSupply` in
    NuCypher's `StakingEscrow` contract. For testing purposes (`developemnt`
    and other testnet networks eg. `ropsten`) we use `totalSuppy()` of mocked
    NU token.

--- deploy/04_deploy_vending_machine.ts
+++ deploy/04_deploy_vending_machine.ts
@@ -1,72 +1,116 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 
-interface VendingMachineDeploymentOptions {
-  tokenArtifactName: string
-  vendingMachineArtifactName: string
-}
+abstract class TVendingMachineDeployer {
+  protected hre: HardhatRuntimeEnvironment
+  protected wrappedTokenArtifactName: string
+  protected vendingMachineArtifactName: string
 
-type VendingMachineTypes = "KEEP" | "NU"
-
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { getNamedAccounts, deployments } = hre
-  const { deployer } = await getNamedAccounts()
-  const { read } = deployments
-
-  const T = await deployments.get("T")
-  const tTotalSupply = await read("T", "totalSupply")
-
-  // We're wrapping 100% of the minted KEEP and will be allocating 45% of the
-  // minted T tokens. The remaining T tokens will be in the future distributed
-  // between another instance of the VendingMachine (which will be wrapping NU
-  // token) and a DAO treasury.
-  const T_VENDING_MACHINE_ALLOCATION = tTotalSupply.mul(45).div(100)
-
-  const VENDING_MACHINE_OPTIONS: Record<
-    VendingMachineTypes,
-    VendingMachineDeploymentOptions
-  > = {
-    KEEP: {
-      tokenArtifactName: "KeepToken",
-      vendingMachineArtifactName: "VendingMachineKeep",
-    },
-    NU: {
-      tokenArtifactName: "NuCypherToken",
-      vendingMachineArtifactName: "VendingMachineNuCypher",
-    },
+  constructor(
+    _hre: HardhatRuntimeEnvironment,
+    _wrappedTokenArtifactName: string,
+    _vendingMachineArtifactName: string
+  ) {
+    this.hre = _hre
+    this.wrappedTokenArtifactName = _wrappedTokenArtifactName
+    this.vendingMachineArtifactName = _vendingMachineArtifactName
   }
 
-  const deployVendingMachine = async function (
-    options: VendingMachineDeploymentOptions
-  ) {
-    const { tokenArtifactName, vendingMachineArtifactName } = options
-    const Token = await deployments.get(tokenArtifactName)
-    const totalSupply = await read(tokenArtifactName, "totalSupply")
+  abstract getWrappedTokenAllocation(): Promise<string>
+
+  async getWrappedTokenAddress(): Promise<string> {
+    const { deployments } = this.hre
+    const Token = await deployments.get(this.wrappedTokenArtifactName)
+    return Token.address
+  }
+
+  async getTTokenAddress(): Promise<string> {
+    const { deployments } = this.hre
+    const T = await deployments.get("T")
+    return T.address
+  }
+
+  async getTTokenAllocation(): Promise<string> {
+    const {
+      deployments: { read },
+    } = this.hre
+
+    const tTotalSupply = await read("T", "totalSupply")
+    return tTotalSupply.mul(45).div(100).toString()
+  }
+
+  async deploy(): Promise<void> {
+    const { deployments, getNamedAccounts } = this.hre
+    const { deployer } = await getNamedAccounts()
+
     const vendingMachine = await deployments.deploy(
-      vendingMachineArtifactName,
+      this.vendingMachineArtifactName,
       {
         contract: "VendingMachine",
         from: deployer,
         args: [
-          Token.address,
-          T.address,
-          totalSupply,
-          T_VENDING_MACHINE_ALLOCATION,
+          await this.getWrappedTokenAddress(),
+          await this.getTTokenAddress(),
+          await this.getWrappedTokenAllocation(),
+          await this.getTTokenAllocation(),
         ],
         log: true,
       }
     )
 
-    if (hre.network.tags.tenderly) {
-      await hre.tenderly.verify({
-        name: vendingMachineArtifactName,
+    if (this.hre.network.tags.tenderly) {
+      await this.hre.tenderly.verify({
+        name: this.vendingMachineArtifactName,
         address: vendingMachine.address,
       })
     }
   }
+}
 
-  await deployVendingMachine(VENDING_MACHINE_OPTIONS.KEEP)
-  await deployVendingMachine(VENDING_MACHINE_OPTIONS.NU)
+class TVendingMachineDeployerKEEP extends TVendingMachineDeployer {
+  constructor(hre: HardhatRuntimeEnvironment) {
+    super(hre, "KeepToken", "VendingMachineKeep")
+  }
+
+  async getWrappedTokenAllocation(): Promise<string> {
+    const {
+      deployments: { read },
+    } = this.hre
+    const totalSupply = await read(this.wrappedTokenArtifactName, "totalSupply")
+    return totalSupply.toString()
+  }
+}
+
+class TVendingMachineDeployerNU extends TVendingMachineDeployer {
+  constructor(hre: HardhatRuntimeEnvironment) {
+    super(hre, "NuCypherToken", "VendingMachineNuCypher")
+  }
+
+  async getWrappedTokenAllocation(): Promise<string> {
+    const { deployments } = this.hre
+    const { read } = deployments
+    if (this.hre.network.name === "mainnet") {
+      const currentPeriodSupply = await read(
+        "NuCypherStakingEscrow",
+        "currentPeriodSupply"
+      )
+      return currentPeriodSupply.toString()
+    }
+
+    const totalSupply = await read(this.wrappedTokenArtifactName, "totalSupply")
+    return totalSupply.toString()
+  }
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const vendingMachineDeployers: TVendingMachineDeployer[] = [
+    new TVendingMachineDeployerKEEP(hre),
+    new TVendingMachineDeployerNU(hre),
+  ]
+
+  for (const vendingMachineDeployer of vendingMachineDeployers) {
+    await vendingMachineDeployer.deploy()
+  }
 }
 
 export default func
