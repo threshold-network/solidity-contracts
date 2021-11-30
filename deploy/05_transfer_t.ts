d1800a2e28880c16cd0ac9a341b0a1c21acd8da8
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-11-30 13:44:10 +0100

    Update the comment in `05_transfer_t.ts`
    Few sentences are out of date after we've added vending machine for NU
    token so this commit will remove them.

--- deploy/05_transfer_t.ts
+++ deploy/05_transfer_t.ts
@@ -20,11 +20,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   // There will be 10B T minted on the production environment. The 45% of this
   // amount will go to the KEEP holders, 45% will go to NU holders and 10% will
-  // be sent to the DAO treasury. In this script we're simulating the part
-  // related to distribution of T to the VendingMachine for KEEP holders.
-  // In the future we'll also have a separate instance of the VendingMachine
-  // (for NU holders), to which we'll send the other 45% of T (4.5B T).
-
+  // be sent to the DAO treasury.
   const T_TO_TRANSFER = to1e18("4500000000") // 4.5B T
 
   for (const { tokenSymbol, vendingMachineAddress } of vendinMachines) {
