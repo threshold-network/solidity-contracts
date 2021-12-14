fb0090cd4620c689027d2ca94fa8e818f5960a12
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-12-14 11:39:59 +0100

    Mint T only if supply is zero
    In case we rerun the scripts we don't want to mint the tokens once
    again, we want to run minting only if tokens supply is zero.

--- deploy/03_mint_t.ts
+++ deploy/03_mint_t.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import type { BigNumber } from "@ethersproject/bignumber"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, helpers } = hre
@@ -9,13 +10,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const T = await deployments.get("T")
 
-  // We're minting 10B T, which is the value of the T supply on the production
-  // environment.
-  const T_SUPPLY = to1e18("10000000000") // 10B T
+  const totalSupply: BigNumber = await read("T", "totalSupply")
 
-  await execute("T", { from: deployer }, "mint", deployer, T_SUPPLY)
+  if (!totalSupply.isZero()) {
+    log(
+      `T tokens were already minted, T total supply: ${from1e18(totalSupply)}`
+    )
+  } else {
+    // We're minting 10B T, which is the value of the T supply on the production
+    // environment.
+    const T_SUPPLY = to1e18("10000000000") // 10B T
 
-  log(`minted ${from1e18(await read("T", "totalSupply"))} T`)
+    await execute("T", { from: deployer }, "mint", deployer, T_SUPPLY)
+
+    log(`minted ${from1e18(await read("T", "totalSupply"))} T`)
+  }
 }
 
 export default func
