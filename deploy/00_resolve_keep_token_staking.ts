7a91a420cf67e7c1fe3a3d21b8ddc384b5d7206b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-12-14 11:45:57 +0100

    Add deployment scripts for TokenStaking contract
    The script requires keep-core V1 Token Staking contract which was added
    as external artifact.
    We first need to deploy KeepStake contract, and finally we can deploy T
    TokenStaking contract.

---
+++ deploy/00_resolve_keep_token_staking.ts
@@ -0,0 +1,31 @@
+import { HardhatRuntimeEnvironment, HardhatNetworkConfig } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments, helpers } = hre
+  const { log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const KeepTokenStaking = await deployments.getOrNull("KeepTokenStaking")
+
+  if (KeepTokenStaking && helpers.address.isValid(KeepTokenStaking.address)) {
+    log(`using existing KeepTokenStaking at ${KeepTokenStaking.address}`)
+  } else if (
+    hre.network.name !== "hardhat" ||
+    (hre.network.config as HardhatNetworkConfig).forking.enabled
+  ) {
+    throw new Error("deployed KeepTokenStaking contract not found")
+  } else {
+    log(`deploying KeepTokenStaking stub`)
+
+    await deployments.deploy("KeepTokenStaking", {
+      contract: "KeepTokenStakingMock",
+      from: deployer,
+      log: true,
+    })
+  }
+}
+
+export default func
+
+func.tags = ["KeepTokenStaking"]
