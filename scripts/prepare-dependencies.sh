1c6064298b9e6fc79a13f52eb1776b83c6108ce3
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-12-28 15:02:26 +0100

    Pass NPM's working directory path to postinstall script
    The script doesn't work correctly when the package is used in another
    project as a dependency. Resolution of subdependend packages fails due
    to the missing files as yarn flattens the node_modules structure. Here
    we want to workaround it.

--- scripts/prepare-dependencies.sh
+++ scripts/prepare-dependencies.sh
@@ -9,8 +9,19 @@ set -eo pipefail
 # This is a workaround for hardhat-deploy plugin limitation:
 # https://github.com/wighawag/hardhat-deploy/issues/241
 
-SOURCE_DIR="node_modules/@keep-network/keep-core/artifacts"
-DESTINATION_DIR="external/npm/@keep-network/keep-core"
+printf "Preparing dependencies artifacts\n"
+
+# If scripts is invoked in other projects during NPM package dependency installation
+# it may be needed to specify path for the module (e.g. when using Yarn).
+ROOT_DIR=${1:-$(realpath $(dirname $0)/../)}
+
+printf "Root directory: $ROOT_DIR\n"
+
+SOURCE_DIR="$ROOT_DIR/node_modules/@keep-network/keep-core/artifacts"
+DESTINATION_DIR="$ROOT_DIR/external/npm/@keep-network/keep-core"
+
+printf "Source directory: $SOURCE_DIR\n"
+printf "Destination directory: $DESTINATION_DIR\n"
 
 mkdir -p $DESTINATION_DIR
 cp -r $SOURCE_DIR $DESTINATION_DIR
