34910d1d3240212ea92bd740118904244dec2405
Manuel Montenegro <manumonti@gmail.com>
2022-01-15 01:38:56 +0100

    Add Staking tests "when operator have no stake"

--- test/stakingEscrow/StakingEscrow.test.js
+++ test/stakingEscrow/StakingEscrow.test.js
@@ -1,20 +1,17 @@
 const { expect } = require("chai")
-const fc = require("fast-check");
+const fc = require("fast-check")
 
 const { helpers } = require("hardhat")
-const { to1e18 } = helpers.number
 const { impersonateAccount } = helpers.account
 const { resetFork } = helpers.forking
 
 const { initContracts } = require("./init-contracts")
-const { daoAgentAddress } = require("./constants")
+const { daoAgentAddress, startingBlock, stakers } = require("./constants")
 
 const describeFn =
   process.env.NODE_ENV === "stakingescrow-test" ? describe : describe.skip
 
 describeFn("System Tests: StakingEscrow", () => {
-  const startingBlock = 13979631
-
   let purse
   let daoAgent
 
@@ -24,20 +21,26 @@ describeFn("System Tests: StakingEscrow", () => {
   let stakingEscrow
   let stakingEscrowImplementation
 
-  beforeEach(async () => {
-    await resetFork(startingBlock)
-
-    purse = await ethers.getSigner(1)
-    daoAgent = await impersonateAccount(daoAgentAddress, {
+  async function impersonate(purse, account) {
+    return await impersonateAccount(account, {
       from: purse,
       value: "10",
     })
+  }
+
+  beforeEach(async () => {
+    fc.configureGlobal({ numRuns: 10 })
+
+    await resetFork(startingBlock)
 
     const contracts = await initContracts()
     nuCypherToken = contracts.nuCypherToken
     tokenStaking = contracts.tokenStaking
     stakingEscrow = contracts.stakingEscrow
     stakingEscrowImplementation = contracts.stakingEscrowImplementation
+
+    purse = await ethers.getSigner(1)
+    daoAgent = await impersonate(purse, daoAgentAddress)
 
     await contracts.stakingEscrowDispatcher
       .connect(daoAgent)
@@ -53,7 +56,7 @@ describeFn("System Tests: StakingEscrow", () => {
       })
 
       it("should prev. target match with target before upgrade", async () => {
-        let previousTarget = await stakingEscrow.previousTarget()
+        const previousTarget = await stakingEscrow.previousTarget()
         await resetFork(startingBlock)
         expect(await stakingEscrow.target()).to.equal(previousTarget)
       })
@@ -61,8 +64,9 @@ describeFn("System Tests: StakingEscrow", () => {
       it("should stakers amount not be zero", async () => {
         expect(await stakingEscrow.getStakersLength()).to.not.equal(0)
       })
-      it("should stakers number match with before upgrade", async () => {
-        let stakersNumber = await stakingEscrow.getStakersLength()
+
+      it("should stakers number match before upgrade", async () => {
+        const stakersNumber = await stakingEscrow.getStakersLength()
         await resetFork(startingBlock)
         expect(await stakingEscrow.getStakersLength()).to.equal(stakersNumber)
       })
@@ -70,23 +74,62 @@ describeFn("System Tests: StakingEscrow", () => {
   })
 
   describe("staking", () => {
-    context("when I have not stake", () => {
-      it("should be able to withdraw NU", async () => {
-        let operatorAddress = await stakingEscrow.stakers(0)
+    context("when operator have no stake on token staking", () => {
+      it("staked NU should be zero", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.integer({ min: 0, max: stakers.length - 1 }),
+            async (index) => {
+              const operatorAddress = stakers[index]
+              expect(await tokenStaking.stakedNu(operatorAddress)).to.equal(0)
+            }
+          )
+        )
+      })
 
-        operator = await impersonateAccount(operatorAddress, {
-          from: purse,
-          value: "10",
-        })
+      it("withdraw should reduce the amount of NU properly", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.integer({ min: 0, max: stakers.length - 1 }),
+            async (index) => {
+              const operatorAddress = stakers[index]
+              const operator = await impersonate(purse, operatorAddress)
 
-        expect(await tokenStaking.stakedNu(operator.address)).to.equal(0)
-        expect(await stakingEscrow.getAllTokens(operator.address)).to.not.equal(0)
-        
-        console.log(await stakingEscrow.getAllTokens(operator.address))
-        console.log(await nuCypherToken.balanceOf(operator.address))
-        await stakingEscrow.connect(operator).withdraw(to1e18(10));
-        console.log(await stakingEscrow.getAllTokens(operator.address))
-        console.log(await nuCypherToken.balanceOf(operator.address))
+              const tokens = await stakingEscrow.getAllTokens(operatorAddress)
+              const withdrawTokens = tokens.div(2)
+              const difference = tokens.sub(withdrawTokens)
+
+              await stakingEscrow.connect(operator).withdraw(withdrawTokens)
+
+              expect(
+                await stakingEscrow.getAllTokens(operatorAddress)
+              ).to.equal(difference)
+            }
+          )
+        )
+      })
+
+      it("withdrawn NU should be transfered to account", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.integer({ min: 0, max: stakers.length - 1 }),
+            async (index) => {
+              const operatorAddress = stakers[index]
+              const operator = await impersonate(purse, operatorAddress)
+
+              const stakeTokens = await stakingEscrow.getAllTokens(
+                operatorAddress
+              )
+              const nuBalance = await nuCypherToken.balanceOf(operatorAddress)
+
+              await stakingEscrow.connect(operator).withdraw(stakeTokens)
+
+              expect(await nuCypherToken.balanceOf(operatorAddress)).to.equal(
+                nuBalance.add(stakeTokens)
+              )
+            }
+          )
+        )
       })
     })
   })
