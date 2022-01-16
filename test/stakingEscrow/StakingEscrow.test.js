d50a1e9d6c7e39dca2a3e73d045be9a16b4e83cf
Manuel Montenegro <manumonti@gmail.com>
2022-01-16 21:33:44 +0100

    Add tests related to operator unstaked all NU
    Also added a explanation about numRuns fast-check property.

--- test/stakingEscrow/StakingEscrow.test.js
+++ test/stakingEscrow/StakingEscrow.test.js
@@ -43,6 +43,10 @@ describeFn("System Tests: StakingEscrow", () => {
   }
 
   before(async () => {
+    // numRuns: is the number of runs of property-based tests. The max allowed
+    // value is stakers.length, which implies an exhaustive testing (all stakers
+    // of Staking Escrow contract are tested). Lower values speed up the tests,
+    // but only that number of stakers (randomly selected) will be tested.
     fc.configureGlobal({ numRuns: stakers.length, skipEqualValues: true })
     await resetFork(startingBlock)
     const contracts = await initContracts()
@@ -229,9 +233,9 @@ describeFn("System Tests: StakingEscrow", () => {
             }
           )
         )
-      }).timeout(120000)
+      }).timeout(300000)
 
-      it("should not be able to withdraw NU", async () => {
+      it("staker should not be able to withdraw NU", async () => {
         await fc.assert(
           fc.asyncProperty(
             fc.integer({ min: 0, max: stakers.length - 1 }),
@@ -255,7 +259,7 @@ describeFn("System Tests: StakingEscrow", () => {
     })
 
     context("when operator partially stake NU", () => {
-      it("should be able to withdraw not staked NU", async () => {
+      it("staker should be able to withdraw not staked NU", async () => {
         await fc.assert(
           fc.asyncProperty(
             fc.integer({ min: 0, max: stakers.length - 1 }),
@@ -271,28 +275,32 @@ describeFn("System Tests: StakingEscrow", () => {
                   .connect(operator)
                   .stakeNu(operatorAddress, operatorAddress, operatorAddress)
                 // Unstake the half of Nu staked
-                await tokenStaking.connect(operator).unstakeNu(operatorAddress, nuToUnstakeInT.tAmount)
+                await tokenStaking
+                  .connect(operator)
+                  .unstakeNu(operatorAddress, nuToUnstakeInT.tAmount)
 
                 const stakedNu = await tokenStaking.stakedNu(operatorAddress)
                 const withdrawableNu = escrowNu.sub(stakedNu)
 
                 await stakingEscrow.connect(operator).withdraw(withdrawableNu)
 
-                expect(await stakingEscrow.getAllTokens(operatorAddress)).to.equal(stakedNu)
+                expect(
+                  await stakingEscrow.getAllTokens(operatorAddress)
+                ).to.equal(stakedNu)
               }
             }
           )
         )
       })
 
-      it("should not be able to withdraw staked NU", async () => {
+      it("staker should not be able to withdraw staked NU", async () => {
         await fc.assert(
           fc.asyncProperty(
             fc.integer({ min: 0, max: stakers.length - 1 }),
             async (index) => {
               const operatorAddress = stakers[index]
               const operator = await impersonate(purse, operatorAddress)
-              let escrowNu = await stakingEscrow.getAllTokens(operatorAddress)
+              const escrowNu = await stakingEscrow.getAllTokens(operatorAddress)
               const nuToUnstakeInT = nuToT(escrowNu.div(2))
 
               // Method reverts if insufficient tokens: skipping this case...
@@ -301,14 +309,80 @@ describeFn("System Tests: StakingEscrow", () => {
                   .connect(operator)
                   .stakeNu(operatorAddress, operatorAddress, operatorAddress)
                 // Unstake the half of Nu staked
-                await tokenStaking.connect(operator).unstakeNu(operatorAddress, nuToUnstakeInT.tAmount)
+                await tokenStaking
+                  .connect(operator)
+                  .unstakeNu(operatorAddress, nuToUnstakeInT.tAmount)
 
-                await expect(stakingEscrow.connect(operator).withdraw(escrowNu)).to.be.reverted
+                await expect(stakingEscrow.connect(operator).withdraw(escrowNu))
+                  .to.be.reverted
               }
             }
           )
         )
       })
+    })
+
+    context("when operator unstake all previously staked Nu", () => {
+      it("staker should be able to withdraw all NU", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.integer({ min: 0, max: stakers.length - 1 }),
+            async (index) => {
+              const operatorAddress = stakers[index]
+              const operator = await impersonate(purse, operatorAddress)
+              const escrowNu = await stakingEscrow.getAllTokens(operatorAddress)
+              // Method reverts if insufficient tokens: skipping this case...
+              if (nuToT(escrowNu).tAmount > 0) {
+                await tokenStaking
+                  .connect(operator)
+                  .stakeNu(operatorAddress, operatorAddress, operatorAddress)
+                await tokenStaking.connect(operator).unstakeAll(operatorAddress)
+                await stakingEscrow.connect(operator).withdraw(escrowNu)
+                expect(await tokenStaking.stakedNu(operatorAddress)).to.equal(0)
+                expect(
+                  await stakingEscrow.getAllTokens(operatorAddress)
+                ).to.equal(0)
+              }
+            }
+          )
+        )
+      })
+
+      it("other operator should be able to stake again", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.integer({ min: 0, max: stakers.length - 1 }),
+            async (index) => {
+              const ownerAddress = stakers[index]
+              const owner = await impersonate(purse, ownerAddress)
+              const escrowNu = await stakingEscrow.getAllTokens(ownerAddress)
+              const otherOperatorAdd = ethers.Wallet.createRandom().address
+
+              // Method reverts if insufficient tokens: skipping this case...
+              if (nuToT(escrowNu).tAmount > 0) {
+                await tokenStaking
+                  .connect(owner)
+                  .stakeNu(ownerAddress, ownerAddress, ownerAddress)
+                await tokenStaking.connect(owner).unstakeAll(ownerAddress)
+                await tokenStaking
+                  .connect(owner)
+                  .stakeNu(otherOperatorAdd, otherOperatorAdd, otherOperatorAdd)
+
+                const roles = await tokenStaking.rolesOf(otherOperatorAdd)
+                const [, , nuInTStake] = await tokenStaking.stakes(
+                  otherOperatorAdd
+                )
+
+                expect(roles.owner).to.equal(ownerAddress)
+                expect(roles.beneficiary).to.equal(otherOperatorAdd)
+                expect(roles.authorizer).to.equal(otherOperatorAdd)
+                expect(await tokenStaking.stakedNu(ownerAddress)).to.equal(0)
+                expect(nuInTStake).to.equal(nuToT(escrowNu).tAmount)
+              }
+            }
+          )
+        )
+      }).timeout(300000)
     })
   })
 })
