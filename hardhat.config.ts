f6335203b3d77b7b44953fa1dff342875a587c89
Piotr Dyraga <piotr.dyraga@keep.network>
2022-05-03 13:30:38 +0200

    Removed hardhat-local-networks-config plugin
    After an update in `deepmerge` library, `hardhat-local-networks-config`
    stopped working and that lead to our system tests to fail with a
    mysterious `TypeError: config.chains.has is not a function` error.
    
    Until the problem is not fixed in `hardhat-local-networks-config`, the
    plugin is removed. It was here for convenience and all tests are passing
    without it.
    
    See https://github.com/NomicFoundation/hardhat/issues/2630

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 
 import "@keep-network/hardhat-helpers"
-import "@keep-network/hardhat-local-networks-config"
 import "@nomiclabs/hardhat-waffle"
 import "@openzeppelin/hardhat-upgrades"
 import "@tenderly/hardhat-tenderly"
