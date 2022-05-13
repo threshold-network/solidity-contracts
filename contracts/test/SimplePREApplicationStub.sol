d3e7c03324d9befa1c9a6c21c79f5ccc949425d8
michalsmiarowski <michal.smiarowski@thesis.co>
2022-05-13 12:17:16 +0200

    Update SimplePREApplicationStub.sol
    Update SimplePREApplicationStub contract by giving it StakingProviderInfo
    struct and add modifying confirmOperatorAddress function to set teh
    StakingProviderInfo map with corect values of operator and stakingProvider.

--- contracts/test/SimplePREApplicationStub.sol
+++ contracts/test/SimplePREApplicationStub.sol
@@ -8,7 +8,20 @@ contract SimplePREApplicationStub {
         address indexed operator
     );
 
+    struct StakingProviderInfo {
+        address operator;
+        bool operatorConfirmed;
+        uint256 operatorStartTimestamp;
+    }
+
+    mapping (address => StakingProviderInfo) public stakingProviderInfo;
+
     function confirmOperatorAddress(address stakingProvider) external {
+        StakingProviderInfo storage info = stakingProviderInfo[stakingProvider];
+        require(!info.operatorConfirmed, "Operator address is already confirmed");
+        info.operator = msg.sender;
+        info.operatorStartTimestamp = block.timestamp;
+        info.operatorConfirmed = true;
         emit OperatorConfirmed(stakingProvider, msg.sender);
     }
 }
