a32e000a95e169268647534946e47b2979436d31
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-06-30 15:25:43 +0200

    Fix formatting

--- deploy/00_resolve_keep_token_staking.ts
+++ deploy/00_resolve_keep_token_staking.ts
@@ -11,7 +11,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (KeepTokenStaking && helpers.address.isValid(KeepTokenStaking.address)) {
     log(`using existing KeepTokenStaking at ${KeepTokenStaking.address}`)
   } else if (
-    (!hre.network.tags.allowStubs) || 
+    !hre.network.tags.allowStubs ||
     (hre.network.config as HardhatNetworkConfig)?.forking.enabled
   ) {
     throw new Error("deployed KeepTokenStaking contract not found")
