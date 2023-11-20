1f6493beba65c76d6dc4e5fcdf2af92e9962b7df
David Núñez <david@nucypher.com>
2023-11-20 11:02:36 +0100

    Update TokenStaking upgrade verification script
    After #153, which drop support for legacy stakers, TokenStaking constructor requires less arguments

--- deploy/60_validate_upgrade_token_staking.ts
+++ deploy/60_validate_upgrade_token_staking.ts
@@ -8,20 +8,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { log } = deployments
 
   const T = await deployments.get("T")
-  const KeepTokenStaking = await deployments.get("KeepTokenStaking")
-  const NuCypherStakingEscrow = await deployments.get("NuCypherStakingEscrow")
-  const VendingMachineKeep = await deployments.get("VendingMachineKeep")
   const VendingMachineNuCypher = await deployments.get("VendingMachineNuCypher")
-  const KeepStake = await deployments.get("KeepStake")
   const TokenStakingDeployment = await deployments.get("TokenStaking")
 
   const tokenStakingConstructorArgs = [
     T.address,
-    KeepTokenStaking.address,
-    NuCypherStakingEscrow.address,
-    VendingMachineKeep.address,
     VendingMachineNuCypher.address,
-    KeepStake.address,
   ]
 
   if (hre.network.name == "mainnet") {
